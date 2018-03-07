72089ed595e530f2055bf77270f05ac8d29a5533
Arie Wolf <awprogramming@gmail.com>
2018-03-07 16:37:06 -0500

    dev tests

--- client/src/app/components/evaluations/evaluations.component.ts
+++ client/src/app/components/evaluations/evaluations.component.ts
@@ -295,8 +295,8 @@ export class EvaluationsComponent implements OnInit {
   }
 
   getType(){
-    console.log(localStorage.getItem('user'));
-    
+    console.log(JSON.parse(localStorage.getItem('user'));
+
     return JSON.parse(localStorage.getItem('user')).type.type;
   }
 
