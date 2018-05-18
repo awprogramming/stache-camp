fdd2d503fa1b382c0ed002c558a747bb6982142f
Arie Wolf <awprogramming@gmail.com>
2018-05-18 12:43:38 -0400

    dev

--- env.js
+++ env.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 if (fs.existsSync('./public')) {
   process.env.NODE_ENV = 'production';
-  process.env.databaseUri = 'mongodb://ariewolf:Carling!123@ds225608.mlab.com:25608/stache-camp'; // Databse URI and database name
+  process.env.databaseUri = 'mongodb://ariewolf:carling1234@ds229130-a0.mlab.com:29130,ds229130-a1.mlab.com:29130/stache-camp?replicaSet=rs-ds229130'; // Databse URI and database name
   process.env.databaseName = 'stache-camp'; // Database name
 } else {
   process.env.NODE_ENV = 'development';
