bf9ef005fe55487cd60ab106f114745ec4092b54
Arie Wolf <awprogramming@gmail.com>
2018-06-27 00:38:23 -0400

    dev

--- cron.js
+++ cron.js
@@ -11,26 +11,33 @@ var transporter = nodemailer.createTransport({
     auth: {
            user: 'tylerhillsports@gmail.com',
            pass: 'THC!2018'
-       }
+       },
+    pool:true
    });
 
-new CronJob('0 0 23 * * *', function() {
+ new CronJob('0 0 16 * * *', function() {
    Camp.find({}, (err, camps)=>{
             console.log("***CRON START***");
             for(let camp of camps){
                 for(let game of camp.games){
                     var three_before = new Date(game.date);
-                    three_before.setDate(three_before.getDate()-3);
-                    if(new Date().getDate() == three_before.getDate()){
+                    three_before.setDate(three_before.getDate()-1);
+                    if(new Date().getDate() == three_before.getDate() && new Date().getFullYear() == three_before.getFullYear()&& new Date().getMonth() == three_before.getMonth()){
                         //CHECK FOR RIGHT DATE
                         console.log("**GAME***")
                         for(let hs of game.specialty.head_specialists){
                             sendRoster(hs.email,game,camp);
+                            //sendRoster("cadetboys@tylerhillcamp.com",game,camp);
                         }
-                        if(game.needsLunch)
+                        if(game.needsLunch){
                             sendKitchen(["cadetboys@tylerhillcamp.com","pete61731880@yahoo.com","food@tylerhillcamp.com"],game,camp); //ADD OPTION TO ADD KITCHEN EMAIL
-                        if(game.location != "home")
+                            //sendKitchen(["cadetboys@tylerhillcamp.com"],game,camp); //ADD OPTION TO ADD KITCHEN EMAIL
+                        }
+                        if(game.location != "home"){
+                            console.log("AWAY GAME");
                             sendHealthCenter(["cadetboys@tylerhillcamp.com","health@tylerhillcamp.com"],game,camp); //ADD OPTION TO ADD HEALTH CENTER EMAIL
+                            //sendHealthCenter(["cadetboys@tylerhillcamp.com"],game,camp); //ADD OPTION TO ADD HEALTH CENTER EMAIL
+                        }
                         
                         if(game.rosterId){
                             var roster = camp.specialties.id(game.specialty._id).rosters.id(game.rosterId);
@@ -52,7 +59,7 @@ new CronJob('0 0 23 * * *', function() {
             console.log("***CRON END***");
         });
 
-}, null, true, 'America/New_York');
+ }, null, true, 'America/New_York');
 
 
 
@@ -60,59 +67,69 @@ function sendHealthCenter(email,game,camp){
     var text = game.name + "\n";
     text += "Roster:"+ "\n";
     var roster = camp.specialties.id(game.specialty._id).rosters.id(game.rosterId);
-    for(let c of roster.campers){
-        var camper = camp.campers.id(c);
-        text += camper.first + " " + camper.last;
-        if(camper.meds){
-            if(camper.meds.epi)
-                text += " (epi)";
-            if(camper.meds.inhaler)
-                text += " (inhaler)";
-            if(camper.meds.other.length!=0){
-                text += " ("
-                for(var i = 0; i <camper.meds.other.length; i++){
-                    text +=camper.meds.other[i].name;
-                    if(i!=camper.meds.other.length-1)
-                        text += ", ";
+    if(roster){
+        for(let c of roster.campers){
+            var camper = camp.campers.id(c);
+            text += camper.first + " " + camper.last;
+            if(camper.meds){
+                if(camper.meds.epi)
+                    text += " (epi)";
+                if(camper.meds.inhaler)
+                    text += " (inhaler)";
+                if(camper.meds.other.length!=0){
+                    text += " ("
+                    for(var i = 0; i <camper.meds.other.length; i++){
+                        text +=camper.meds.other[i].name;
+                        if(i!=camper.meds.other.length-1)
+                            text += ", ";
+                    }
+                    text += ")";
                 }
-                text += ")";
             }
+            text+="\n";
         }
-        text+="\n";
+    }
+    else{
+        text+="No roster submitted";
     }
 
     var html = "<h1>"+game.name+ "</h1>";
     html += "<h2>"+displayDateTime(game.date)+"</h2>";
     html += "<h2>Roster:</h2>";
     var roster = camp.specialties.id(game.specialty._id).rosters.id(game.rosterId);
-    html += "<table style='border:thin solid black;border-collapse:collapse;'>"
-    html += "<tr style='border:thin solid black;border-collapse:collapse;'><th style='border:thin solid black;border-collapse:collapse;'>Name</th><th style='border:thin solid black;border-collapse:collapse;'>Epi</th><th style='border:thin solid black;border-collapse:collapse;'>Inhaler</th><th style='border:thin solid black;border-collapse:collapse;'>Other</th></tr>"
-    for(let c of roster.campers){
-        html += "<tr style='border:thin solid black;border-collapse:collapse;'>";
-        var camper = camp.campers.id(c);
-        html += "<td>";
-        html += camper.first + " " + camper.last;
-        html += "</td>";
-        html += "<td>";
-        if(camper.meds.epi)
-            html += "✔";
-        html += "</td>";
-        html += "<td>";
-        if(camper.meds.inhaler)
-            html += "✔";
-        html += "</td>";
-        html += "<td>";
-        if(camper.meds.other.length!=0){
-            for(var i = 0; i <camper.meds.other.length; i++){
-                html +=camper.meds.other[i].name;
-                if(i!=camper.meds.other.length-1)
-                    html += ", ";
+    if(roster){
+        html += "<table style='border:thin solid black;border-collapse:collapse;'>"
+        html += "<tr style='border:thin solid black;border-collapse:collapse;'><th style='border:thin solid black;border-collapse:collapse;'>Name</th><th style='border:thin solid black;border-collapse:collapse;'>Epi</th><th style='border:thin solid black;border-collapse:collapse;'>Inhaler</th><th style='border:thin solid black;border-collapse:collapse;'>Other</th></tr>"
+        for(let c of roster.campers){
+            html += "<tr style='border:thin solid black;border-collapse:collapse;'>";
+            var camper = camp.campers.id(c);
+            html += "<td style='border:thin solid black;border-collapse:collapse;'>";
+            html += camper.first + " " + camper.last;
+            html += "</td>";
+            html += "<td style='border:thin solid black;border-collapse:collapse;'>";
+            if(camper.meds.epi)
+                html += "✔";
+            html += "</td>";
+            html += "<td style='border:thin solid black;border-collapse:collapse;'>";
+            if(camper.meds.inhaler)
+                html += "✔";
+            html += "</td>";
+            html += "<td style='border:thin solid black;border-collapse:collapse;'>";
+            if(camper.meds.other.length!=0){
+                for(var i = 0; i <camper.meds.other.length; i++){
+                    html +=camper.meds.other[i].name;
+                    if(i!=camper.meds.other.length-1)
+                        html += ", ";
+                }
             }
+            html += "</td>";
+            html += "</tr>";
         }
-        html += "</td>";
-        html += "</tr>";
+        html += "</table>"
     }
-    html += "</table>"
+    else{
+        html+="<p>No roster Submitted</p>";
+    }
     let mailOptions = {
         from: '"Game Rosters" <rosters@stachecamp.com>', // sender address
         to: email, // list of receivers
@@ -128,28 +145,33 @@ function sendKitchen(email,game,camp){
     var text = game.name + "\n";
     text += "Roster:"+ "\n";
     var roster = camp.specialties.id(game.specialty._id).rosters.id(game.rosterId);
-    for(let c of roster.campers){
-        var camper = camp.campers.id(c);
-        text += camper.first + " " + camper.last;
-        if(camper.dietary.allergies.length!=0){
-            text += " ("
-            for(var i = 0; i <camper.dietary.allergies.length; i++){
-                text +=camper.dietary.allergies[i].name;
-                if(i!=camper.dietary.allergies.length-1)
-                    text += ", ";
+    if(roster){
+        for(let c of roster.campers){
+            var camper = camp.campers.id(c);
+            text += camper.first + " " + camper.last;
+            if(camper.dietary.allergies.length!=0){
+                text += " ("
+                for(var i = 0; i <camper.dietary.allergies.length; i++){
+                    text +=camper.dietary.allergies[i].name;
+                    if(i!=camper.dietary.allergies.length-1)
+                        text += ", ";
+                }
+                text += ")";
             }
-            text += ")";
-        }
-        if(camper.dietary.other.length!=0){
-            text += " ("
-            for(var i = 0; i <camper.dietary.other.length; i++){
-                text +=camper.dietary.other[i].name;
-                if(i!=camper.dietary.other.length-1)
-                    text += ", ";
+            if(camper.dietary.other.length!=0){
+                text += " ("
+                for(var i = 0; i <camper.dietary.other.length; i++){
+                    text +=camper.dietary.other[i].name;
+                    if(i!=camper.dietary.other.length-1)
+                        text += ", ";
+                }
+                text += ")";
             }
-            text += ")";
+            text+="\n";
         }
-        text+="\n";
+    }
+    else{
+        text+="No roster submitted";
     }
 
     var html = "<style>table,tr,td,th{border:thin solid black;border-collapse:collapse;} td{padding:5px;}</style>";
@@ -157,35 +179,40 @@ function sendKitchen(email,game,camp){
     html += "<h2>"+displayDateTime(game.date)+"</h2>";
     html += "<h2>Roster:</h2>";
     var roster = camp.specialties.id(game.specialty._id).rosters.id(game.rosterId);
-    html += "<table>"
-    html += "<tr style='border:thin solid black;border-collapse:collapse;'><th style='border:thin solid black;border-collapse:collapse;'>Name</th><th style='border:thin solid black;border-collapse:collapse;'>Allergies</th><th style='border:thin solid black;border-collapse:collapse;'>Other</th></tr>"
-    for(let c of roster.campers){
-        html += "<tr style='border:thin solid black;border-collapse:collapse;'>"
-        var camper = camp.campers.id(c);
-        html += "<td>"
-        html += camper.first + " " + camper.last;
-        html += "</td>"
-        html += "<td>"
-        if(camper.dietary.allergies.length!=0){
-            for(var i = 0; i <camper.dietary.allergies.length; i++){
-                html +=camper.dietary.allergies[i].name;
-                if(i!=camper.dietary.allergies.length-1)
-                    html += ", ";
+    if(roster){
+        html += "<table>"
+        html += "<tr style='border:thin solid black;border-collapse:collapse;'><th style='border:thin solid black;border-collapse:collapse;'>Name</th><th style='border:thin solid black;border-collapse:collapse;'>Allergies</th><th style='border:thin solid black;border-collapse:collapse;'>Other</th></tr>"
+        for(let c of roster.campers){
+            html += "<tr style='border:thin solid black;border-collapse:collapse;'>"
+            var camper = camp.campers.id(c);
+            html += "<td>"
+            html += camper.first + " " + camper.last;
+            html += "</td>"
+            html += "<td>"
+            if(camper.dietary.allergies.length!=0){
+                for(var i = 0; i <camper.dietary.allergies.length; i++){
+                    html +=camper.dietary.allergies[i].name;
+                    if(i!=camper.dietary.allergies.length-1)
+                        html += ", ";
+                }
             }
-        }
-        html += "</td>"
-        html += "<td>"
-        if(camper.dietary.other.length!=0){
-            for(var i = 0; i <camper.dietary.other.length; i++){
-                html +=camper.dietary.other[i].name;
-                if(i!=camper.dietary.other.length-1)
-                    html += ", ";
+            html += "</td>"
+            html += "<td>"
+            if(camper.dietary.other.length!=0){
+                for(var i = 0; i <camper.dietary.other.length; i++){
+                    html +=camper.dietary.other[i].name;
+                    if(i!=camper.dietary.other.length-1)
+                        html += ", ";
+                }
             }
+            html += "</td>"
+            html += "</tr>";
         }
-        html += "</td>"
-        html += "</tr>";
+        html += "</table>"
     }
-    html += "</table>"
+    else{
+        html+="<p>No roster Submitted</p>";
+    }
     let mailOptions = {
         from: '"Game Rosters" <rosters@stachecamp.com>', // sender address
         to: email, // list of receivers
@@ -201,28 +228,41 @@ function sendRoster(email,game,camp){
     var text = game.name + "\n";
     text += "Roster:"+ "\n";
     var roster = camp.specialties.id(game.specialty._id).rosters.id(game.rosterId);
-    for(let c of roster.campers){
-        var camper = camp.campers.id(c);
-        text += camper.first + " " + camper.last;
-        text+="\n";
+    if(roster){
+        for(let c of roster.campers){
+            var camper = camp.campers.id(c);
+            text += camper.first + " " + camper.last;
+            text+="\n";
+        }
+    }
+    else{
+        text+="No roster submitted";
     }
 
     var html = "<style>table,tr,td,th{border:thin solid black;border-collapse:collapse;} td{padding:5px;}</style>";
     html += "<h1>"+game.name+ "</h1>";
     html += "<h2>"+displayDateTime(game.date)+"</h2>";
+    html += "<h2>Location: "+game.location+"</h2>";
     html += "<h2>Roster:</h2>";
+
     var roster = camp.specialties.id(game.specialty._id).rosters.id(game.rosterId);
-    html += "<table>"
-    for(let c of roster.campers){
-        html += "<tr style='border:thin solid black;border-collapse:collapse;'>"
-        var camper = camp.campers.id(c);
-        html += "<td>"
-        html += camper.first + " " + camper.last;
-        html += "</td>"
-        html += "</tr>";
+    if(roster){
+        html += "<table>";
+        for(let c of roster.campers){
+                html += "<tr style='border:thin solid black;border-collapse:collapse;'>"
+                var camper = camp.campers.id(c);
+                html += "<td>"
+                html += camper.first + " " + camper.last;
+                html += "</td>"
+                html += "</tr>";
+            }
+        
+        html += "</table>"
+        html += "<h2>Coaches:</h2>";
     }
-    html += "</table>"
-    html += "<h2>Coaches:</h2>";
+    else{
+        html+="<p>No roster Submitted</p>";
+    }
     if(game.coachIds.length == 0){
         html += "<span>No Refs Pre-Selected</span>"
     }
@@ -282,10 +322,19 @@ function sendEmail(mailOptions,message){
 
 function displayDateTime(date){
     var d = new Date(date)
-    var time = d.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
-    var day = this.getDayName(d.getDay());
-    var month = this.getMonthName(d.getMonth());
+    var time = d.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit',hour12:true});
+    var day = getDayName(d.getDay());
+    var month = getMonthName(d.getMonth());
     return time + " on " + day+ ", " + month + " " + d.getDate();
-
 }
+
+function getMonthName(month){
+    var monthNames = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    return monthNames[month];
+  }
+
+function getDayName(day){
+    var dayNames = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    return dayNames[day];
+  }
 // });
--- public/main.bundle.js
+++ public/main.bundle.js
@@ -43244,7 +43244,8 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var AuthService = (function () {
     function AuthService(http) {
         this.http = http;
-        this.domain = ""; // Production;
+        //domain = ""; // Production;
+        this.domain = "http://localhost:8080/";
     }
     // Function to create headers, add token, to be used in HTTP requests
     AuthService.prototype.createAuthenticationHeaders = function () {
